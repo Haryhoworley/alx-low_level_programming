421e24277b73f29ba61fc18c13a4efbcc6ce5658
Haryhoworley <bayooduwole999@gmail.com>
2022-04-19 14:32:18 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - Prints the number zero to nine
- * 
+ *
  * Return: Always 0 (Success)
  */
 
@@ -11,7 +11,7 @@ void print_numbers(void)
 
 int i;
 
-i = 0;
+i = 0
 
 for (i = 0; i < 10; i++)
 
@@ -19,4 +19,3 @@ _putchar(i + '0');
 
 _putchar ('\n');
 }
-
