b5c0f1b85a3cc1e5fb69afbcd339cf7c26f48b5c
Haryhoworley <bayooduwole999@gmail.com>
2022-04-26 16:17:14 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int _putchar(char c);
+char *_memeset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *strstr(char *haystick, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char ***s, char *to);
+
+#endif
